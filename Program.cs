38c6ec2942439ec7f4dd863178d28dc93dc9cafe
DavideBiag <149298748+DavideBiag@users.noreply.github.com>
2023-11-29 18:43:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            quadrato q1 = new quadrato();
+
+            
+            Console.WriteLine("inserisci il lato del quadrato:");
+            q1.lato_quadrato=float.Parse(Console.ReadLine());
+            Console.WriteLine( "il quadrato ha superfice = {0}", q1.lato_quadrato*q1.lato_quadrato);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ quadrato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._1
+{
+    internal class quadrato
+    {
+
+
+        public float lato_quadrato;
+
+        public quadrato()
+        {
+        }
+
+        public quadrato(float lato_quadrato)
+        {
+            this.lato_quadrato = lato_quadrato;
+        }
+    }
+}
